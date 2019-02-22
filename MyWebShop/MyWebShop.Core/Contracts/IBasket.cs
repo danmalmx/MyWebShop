ced83e9c0cae519537a852a449b9b5bc3c5dad03
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-22 15:06:49 +0100

    adding basket service 2/2

---
+++ MyWebShop/MyWebShop.Core/Contracts/IBasket.cs
@@ -0,0 +1,18 @@
+﻿using MyWebShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyWebShop.Core.Contracts
+{
+    public interface IBasket
+    {
+        void AddtoBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+    }
+}
